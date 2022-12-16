6860a1e1b97c2c01a799a3fc20086ab5cfc81137
Erlend Hårstad <erha@equinor.com>
2022-12-16 15:06:54 +0100

    alternative c++ structure

--- internal/vds/vdshandle.h
+++ internal/vds/vdshandle.h
@@ -26,6 +26,167 @@ struct BoundingBox {
     DoublePointList world;
 };
 
+class AxisDescr {
+    int dimension;
+
+    int min;
+    int max;
+    int stride;
+    int samples;
+
+    const char * name;
+    const char * unit;
+};
+
+// Implementation of Axis that knows its own string representation
+// and which coordinate system it belongs too.
+// TODO naming conflict with current Axis
+struct Axis {
+    Axis(CAxis axis) : axis(axis) {}
+
+    CordinateSystem system() const {
+        switch (this->value) {
+            /* ... */
+        }
+    }
+
+    std::string string() const { /* ... */ }
+};
+
+private:
+    CAxis axis;
+}
+
+struct SeismicAxisMap {
+    virtual int iline()  const = 0;
+    virtual int xline()  const = 0;
+    virtual int sample() const = 0;
+    virtual int offset() const = 0; // Planing for a future of Prestack support
+};
+
+struct PostStackAxisMap : public SeismicAxisMap {
+public:
+    PostStackAxisMap(int i, int x, int s) : i(i), x(x), s(s) {};
+
+    int iline()  const override { return this->i };
+    int xline()  const override { return this->x };
+    int sample() const override { return this->s };
+    int offset() const override { throw std::not_implemented(""); };
+private:
+    int i;
+    int x;
+    int s;
+};
+
+/** Verifies e.g. annotations is present */
+class SeismicVDSHandle {
+public:
+    SeismicVDSHandle(
+        std::string           url,
+        std::string           connection,
+        struct Channel        default_channel,
+        struct LOD            default_lod,
+        struct SeismicAxisMap axis_map
+    );
+
+    struct SeismicVDSValidator { /* ... */ };
+
+    // Maps from our Axis to a VDS axisDescriptor.
+    AxisDescr   get_axis(Axis axis) const;
+    BoundingBox get_bounding_box() const;
+    std::string get_crs() const;
+    std::string get_format(Channel ch = Channel::None) const;
+private:
+    struct SeismicAxisMap axis_map;
+};
+
+AxisDescr SeismicVDSHandle::get_axis(Axis axis) const {
+    int axis;
+    switch (axis.Value()) {
+        case CAxis::I:
+        case CAxis::Inline: {
+            axis = this->axis_map.iline;
+            break;
+        }
+        // ...
+    }
+
+    return {
+        axis,
+        this->layout.GetDimensionMin(axis),
+        this->layout.GetDimensionMax(axis),
+        // TODO stride
+        this->layout.GetDimensionNumSamples(axis),
+        // TODO add units and name
+    }
+}
+
+
+// Does the actual validation for the PostStack class
+struct PostStackValidator {
+public:
+    // Verify dimensions
+    // Verify axis order
+    bool validate(const PostStack& handle) {
+    }
+
+    bool units(const PostStack& handle) {
+    }
+
+private:
+    // ...
+};
+
+/** Verifies dimension is 3D and axis order */
+class PostStack : SeismicVDSHandle {
+public:
+    PostStack(std::string url, std::string conn)
+        : SeismicVDSHandle(/* ... */, PostStackAxisMap( 2, 1, 0 ))  // hardcode axis map, this assumption will be validated
+    {
+        // TODO hardcode defaults for lod and channel and send to SeismicVDSHandle
+        if not PostStackValidator().validate(this) throw std::runtime_error("");
+    }
+
+    requestdata slice(
+        Axis axis,
+        int lineno,
+        LOD lod = LOD::None,
+        Channel channel = Channel::None
+    );
+
+private:
+    SubVolume slice_as_subvolume(int axis, int lineno);
+    buffer    get_subvolume(SubVolume volume);
+};
+
+requestbuffer PostStack::slice(
+    Axis axis,
+    int lineno,
+    LOD lod = LOD::Default,
+    Channel channel = Channel::Default
+) {
+    AxisDescr description = this->get_axis(axis);
+    CoordinateSystem = Axis(axis).system();
+
+    if (system = CoordinateSystem::Annotation) {
+        lineno = (lineno - axis_description.min) / axis_description.stride;
+    }
+
+    if (not PostStackValidator(this).units())
+        throw std::invalid_argument("");
+
+    if (not this->slice_bounds_check(description.axis, lineno))
+        throw std::out_of_bounds("");
+
+    SubVolume subvolume = this->slice_as_subvolume(axis, lineno);
+    return this->get_subvolume(subvolume, lod, channel);
+}
+
+requestbuffer slice(Axis axis, int lineno) {
+    PostStackHandle poststack(url, conn);
+    return poststack.slice(Axis, lineno);
+}
+
 class VDSHandle {
 
     private:
